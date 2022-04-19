2bae0550ac83cb3a7ce8ceb46b3f71fa51024ff0
hangsiahong <honghangsia@gmail.com>
2022-04-19 03:22:31 -0400

    update

--- examples/ts-node/src/index.ts
+++ examples/ts-node/src/index.ts
@@ -1,31 +1,47 @@
+import * as readline from 'readline';
+
 import { KumandraClient, Identity } from "@kumandra/kumandra";
 import { promises as fs } from "fs";
-
 
 const NODE_WS_PROVIDER = "ws://localhost:9944";
 const FARMER_WS_PROVIDER = "ws://localhost:9955";
 const suri = "//Alice";
 
-const data = await fs.readFile("./sample-input.jpg");
-const objectData = new Uint8Array(data);
+// const data = await fs.readFile("./sample-input.jpg");
+// const objectData = new Uint8Array(data);
 
-// Generate an Identity, we create from a random account for this example.
-const identity = await Identity.fromUri(suri);
+let rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+  });
 
-// Generate a KumandraClient and connect to node and farmer rpc endpoints.
-const kumandraClient = await KumandraClient.connect(
-  identity,
-  NODE_WS_PROVIDER,
-  FARMER_WS_PROVIDER
-);
+rl.question(`Please input your file, you want to upload. `, async upload_data => {
+  console.log(`Hello, File you want to upload is ${upload_data}!`)
+  const data = await fs.readFile(upload_data)
+  const objectData = new Uint8Array(data);
 
-// Put the file as (Uint8Array) in to the objectStore and return the objectId
-const objectId: string = await kumandraClient.putObject(objectData);
-console.log("objectId", objectId);
 
-// Using the objectId get the file as (Uint8Array) from the objectStore.
-const object: Uint8Array = await kumandraClient.getObject(objectId);
+  // Generate an Identity, we create from a random account for this example.
+  const identity = await Identity.fromUri(suri);
+  
+  // Generate a KumandraClient and connect to node and farmer rpc endpoints.
+  const kumandraClient = await KumandraClient.connect(
+    identity,
+    NODE_WS_PROVIDER,
+    FARMER_WS_PROVIDER
+  );
+  
+  // Put the file as (Uint8Array) in to the objectStore and return the objectId
+  const objectId: string = await kumandraClient.putObject(objectData);
+  console.log("objectId", objectId);
+  
+  // Using the objectId get the file as (Uint8Array) from the objectStore.
+  const object: Uint8Array = await kumandraClient.getObject(objectId);
+  
+  // Write the file as a copy from the original
+  await fs.writeFile(data, object, {});
+//   console.log("writeFile ${data}");
+  console.log(data);
+    rl.close()
+})
 
-// Write the file as a copy from the original
-await fs.writeFile("./sample-from-objectStore.jpg", object, {});
-console.log("writeFile ./sample-from-objectStore.jpg");
